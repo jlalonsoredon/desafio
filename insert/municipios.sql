6a4d990334c4412b253a06b7266a529581e65dab
Varushet <160305561+Varushet@users.noreply.github.com>
2026-06-07 15:42:01 +0200

    cambio

---
+++ insert/municipios.sql
@@ -0,0 +1,36 @@
+INSERT INTO shared.municipalities (nombre, provincia, nora_code, province_code, lat, lng) VALUES
+-- Bizkaia (province_code = 48)
+('Bilbao', 'Bizkaia', '014001', 48, 43.2630, -2.9350),
+('Getxo', 'Bizkaia', '014027', 48, 43.3478, -3.0133),
+('Barakaldo', 'Bizkaia', '014007', 48, 43.2967, -2.9878),
+('Portugalete', 'Bizkaia', '014065', 48, 43.3217, -3.0206),
+('Santurtzi', 'Bizkaia', '014078', 48, 43.3289, -3.0331),
+('Basauri', 'Bizkaia', '014008', 48, 43.2367, -2.8889),
+('Leioa', 'Bizkaia', '014041', 48, 43.3267, -2.9889),
+('Galdakao', 'Bizkaia', '014025', 48, 43.2378, -2.8478),
+('Durango', 'Bizkaia', '014020', 48, 43.1700, -2.6333),
+('Erandio', 'Bizkaia', '014022', 48, 43.2978, -2.9667),
+('Sestao', 'Bizkaia', '014080', 48, 43.3111, -3.0089),
+('Amorebieta-Etxano', 'Bizkaia', '014003', 48, 43.2167, -2.7167),
+('Bermeo', 'Bizkaia', '014010', 48, 43.4211, -2.7239),
+('Mungia', 'Bizkaia', '014054', 48, 43.3667, -2.9167),
+('Gernika-Lumo', 'Bizkaia', '014026', 48, 43.3167, -2.6833),
+
+-- Gipuzkoa (province_code = 20)
+('Donostia-San Sebastián', 'Gipuzkoa', '020069', 20, 43.3183, -1.9812),
+('Irun', 'Gipuzkoa', '020036', 20, 43.3389, -1.7889),
+('Errenteria', 'Gipuzkoa', '020062', 20, 43.3167, -1.8978),
+('Eibar', 'Gipuzkoa', '020023', 20, 43.1833, -2.4667),
+('Tolosa', 'Gipuzkoa', '020075', 20, 43.1333, -2.0833),
+('Zarautz', 'Gipuzkoa', '020080', 20, 43.2833, -2.1667),
+('Arrasate/Mondragón', 'Gipuzkoa', '020006', 20, 43.0667, -2.4833),
+('Hernani', 'Gipuzkoa', '020031', 20, 43.2667, -1.9833),
+('Pasaia', 'Gipuzkoa', '020057', 20, 43.3278, -1.9333),
+('Azpeitia', 'Gipuzkoa', '020011', 20, 43.1833, -2.2667),
+
+-- Araba/Álava (province_code = 01)
+('Vitoria-Gasteiz', 'Araba', '001059', 1, 42.8467, -2.6717),
+('Llodio/Laudio', 'Araba', '001033', 1, 43.1500, -2.9667),
+('Amurrio', 'Araba', '001003', 1, 43.0500, -3.0000),
+('Salvatierra/Agurain', 'Araba', '001050', 1, 42.8500, -2.5833),
+('Aramaio', 'Araba', '001005', 1, 43.0167, -2.5833);
